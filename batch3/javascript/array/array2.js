f6755b249a2eece20d98d8d13c017cc1db926472
Suriya Ravichandran <85984478+Suriya-Ravichandran@users.noreply.github.com>
2025-03-04 13:19:57 +0000

    w3clone

---
+++ batch3/javascript/array/array2.js
@@ -0,0 +1,17 @@
+text=["foo","boo","foo boo"]
+text2=[1,2,3,4,5,6,7]
+text3=[2,3,7,6,9,1,0,8]
+
+console.log(text)
+
+console.log(text.toString())
+
+console.log(text.join(text2))
+
+console.log(text.concat(text2))
+
+
+console.log(text2.slice(2,5))
+
+console.log(text3.sort())
+
