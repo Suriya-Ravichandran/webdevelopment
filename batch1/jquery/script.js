31e1993f0256156909d59708ca1d02360a68446e
suriyaravichandran <rsuriya119@gmail.com>
2025-03-15 13:13:41 +0530

    add bootstrap

---
+++ batch1/jquery/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("#menu").click(function(){
+        // $(".menu-list").toggle();
+        $("#menu-list").fadeToggle("slow");
+        // alert("hello I am jquery alert")
+    });
+});
