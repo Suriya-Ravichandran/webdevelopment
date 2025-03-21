0281b4c8ae7ccbcc0d2da6e723e60c0702d0a21d
suriyaravichandran <rsuriya119@gmail.com>
2025-03-21 16:28:56 +0530

    update new

---
+++ batch4/js/loop/triangle.js
@@ -0,0 +1,4 @@
+row=10;
+for(let i=1;i<=row;i++){
+    console.log("x".repeat(i));
+}
---
+++ batch4/js/loop/triangle2.js
@@ -0,0 +1,8 @@
+for(let i=1;i<10;i++)
+{
+    let space="";
+    for(let j=1;j<=i;j++){
+        space+="*";
+    }
+    console.log(space)
+}
