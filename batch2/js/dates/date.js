43db48de3fd596a79de7ca11353afd08ab04c151
suriyaravichandran <rsuriya119@gmail.com>
2025-06-16 19:27:47 +0530

    update

---
+++ batch2/js/dates/date.js
@@ -0,0 +1,2 @@
+const d = new Date("2025-06-09")
+console.log(d)
---
+++ batch2/js/math/math.js
@@ -0,0 +1,5 @@
+// console.log(Math.PI); // Output: 3.141592653589793
+// console.log(Math.sqrt(25)); // Output: 5
+// console.log(Math.round(3.7)); // Output: 4
+let data =Math.random()*10; // Output: a random number between 0 and 1
+console.log(Math.floor(data))
